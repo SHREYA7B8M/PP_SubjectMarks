352830b074dd86ce3fbf55d4346dab811211d91a
s66829020@gmail.com <s66829020@gmail.com>
2023-09-07 18:55:21 +0530

    first commit

---
+++ SubjectMarks/SubjectMarks/Data/SubjectMarksDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SubjectMarks.Models;
+
+namespace SubjectMarks.Data
+{
+    public class SubjectMarksDbContext : DbContext
+    {
+        public SubjectMarksDbContext (DbContextOptions<SubjectMarksDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SubjectMarks.Models.RainbowSchool> RainbowSchool { get; set; } = default!;
+    }
+}
